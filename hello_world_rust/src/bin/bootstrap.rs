2fb1017792f7c9cf1ee953ef3d9001fbe09a2851
Emilio Wuerges <wuerges@gmail.com>
2021-11-01 17:43:02 -0300

    working!

--- hello_world_rust/src/bin/bootstrap.rs
+++ hello_world_rust/src/bin/bootstrap.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use lambda_runtime::{handler_fn, Context, Error};
+use serde_json::{json, Value};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let func = handler_fn(func);
+    lambda_runtime::run(func).await?;
+    Ok(())
+}
+
+async fn func(_: Value, _: Context) -> Result<Value, Error> {
+    // let first_name = event["firstName"].as_str().unwrap_or("world");
+    // Ok(json!({"statusCode": 200, "body": {"message": "hello from Rust!"}}))
+    Ok(json!({
+    "statusCode": 200,
+    "body": "{\"message\": \"hello world from rust\"}",
+    }))
 }
